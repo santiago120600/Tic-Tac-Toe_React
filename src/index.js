0e748e244d6e5f3f4b23870c9bf62a5ad8cb7f9b
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-01-29 12:24:31 -0600

    winning fixed

--- src/index.js
+++ src/index.js
@@ -31,14 +31,15 @@ const Board = () => {
     );
   };
 
-  const winner = calculateWinner(squares);
-  const status = winner ?
-  `winner: ${winner}` :
-  `Next player: ${xIsNext ? 'X' : 'O'}`;
+  calculateWinner(squares);
+  // const winner = calculateWinner(squares);
+  // const status = winner ?
+  // `winner: ${winner}` :
+  // `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   return (
     <div style={{backgroundColor: 'skyblue',margin:10,padding:10}}>
-      <div className="status">{status}</div>
+      {/* <div className="status">{status}</div> */}
       <div className="board-row">
         {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
       </div>
@@ -59,24 +60,25 @@ const Game = () => {
 ReactDom.render(<Game />,document.getElementById('root'));
 
 function calculateWinner(squares){  
-  // dividir en 3 listas
-  var row1 = squares.slice(0,3);
-  var row2 = squares.slice(3,6);
-  var row3 = squares.slice(6,9);
-  var matrix = [row1,row2,row3];
-
-  matrix.forEach(myFunction);
-  function myFunction(row){
-    var col1 = row[0];
-    var col2 = row[1];
-    var col3 = row[2];
-    if (col1 && col1 === col2 && col2 === col3) {
-      if (col1 === 'O'){
-        return 'O';
-      }else{
-        return 'X';
-      }
-
-    }
+ let winning_combos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+ ]
+ for(let i=0; i<winning_combos.length; i++){
+  let winning_row = winning_combos[i]
+  let p1 = winning_row[0]
+  let p2 = winning_row[1]
+  let p3 = winning_row[2]
+  
+  if (squares[p1] && squares[p1]==squares[p2] && squares[p2]==squares[p3] && squares[p3]==squares[p1]) {
+    alert(`winner! player  has won the game`)
   }
+
+ }
 }
